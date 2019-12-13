5675005cf6cc524f9c881a4796d1426665924ba3
Alexander Tsukanov <kerdevid@gmail.com>
2019-12-13 15:02:46 +0300

    first commit

---
+++ src/Controller/Admin/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is a part of PMP project
+ *
+ * Copyright (c) Alexander Tsukanov (http://alexdevid.ru)
+ *
+ * The unauthorized use of this code is prohibited
+ */
+
+namespace PMP\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Alexander Tsukanov (http://alexdevid.ru)
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin.default.index")
+     *
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('admin/default/index.html.twig');
+    }
+}
---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is a part of PMP project
+ *
+ * Copyright (c) Alexander Tsukanov (http://alexdevid.ru)
+ *
+ * The unauthorized use of this code is prohibited
+ */
+
+namespace PMP\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * @author Alexander Tsukanov (http://alexdevid.ru)
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
